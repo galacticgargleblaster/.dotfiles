68e590131b5e7421be86f4d29e0f3bef269464ca
Nicholas Kirkby <nkirkby@e1z2r7p11.42.us.org>
2019-02-13 17:40:15 -0800

    plists are only autoloaded if their names are unique

--- .macos/apply_preferences.sh
+++ .macos/apply_preferences.sh
@@ -10,3 +10,12 @@ defaults -currentHost write -g com.apple.keyboard.modifiermapping.1452-591-0 -ar
 	<real>30064771129</real>
 </dict>'
 
+
+# These key repeat settings unfortunately don't take effect until logging out and logging back in.  Where are these settings persisted?
+
+defaults write com.apple.universalaccess slowKey -int 0 # Disable Slow Keys.
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool FALSE # Disable Press-And-Hold for keys.
+defaults write NSGlobalDomain InitialKeyRepeat -int 10 # Set key repeat triggering delay to even shorter.
+defaults write NSGlobalDomain KeyRepeat -int 3 # Set key repeat rate to even faster.
+
+hostname > ~/yep_it_worked
