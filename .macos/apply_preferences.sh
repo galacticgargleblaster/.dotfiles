d07f0871ce524755856f4499e324c6ecd98f431b
Nicholas Kirkby <nkirkby@e1z4r12p8.42.us.org>
2019-02-14 18:10:53 -0800

    hmm, this shell script from the internet seems to work

--- .macos/apply_preferences.sh
+++ .macos/apply_preferences.sh
@@ -1,14 +1,38 @@
 #!/bin/sh
 
-# https://apple.stackexchange.com/questions/141069/updating-modifier-keys-from-the-command-line-takes-no-effect
-# this maps caps lock to escape globally
-defaults -currentHost write -g com.apple.keyboard.modifiermapping.1452-591-0 -array \
-'<dict>
-	<key>HIDKeyboardModifierMappingDst</key>
-	<real>30064771113</real>
-	<key>HIDKeyboardModifierMappingSrc</key>
-	<real>30064771129</real>
-</dict>'
+# Do these settings persist after logging out and back in?
+# Where are they stored?  When are they read?  What did I have for breakfast?
+
+
+#!/bin/sh
+# Credit to Brad Howes
+FROM='"HIDKeyboardModifierMappingSrc"'
+TO='"HIDKeyboardModifierMappingDst"'
+
+ARGS=""
+function Map # FROM TO
+{
+    CMD="${CMD:+${CMD},}{${FROM}: ${1}, ${TO}: ${2}}"
+}
+
+# Referencing :
+# https://opensource.apple.com/source/IOHIDFamily/IOHIDFamily-1035.41.2/IOHIDFamily/IOHIDUsageTables.h.auto.html
+SECTION="0x700000064"
+ESCAPE="0x700000029"
+BACKQUOTE="0x700000035"
+CAPS_LOCK="0x700000039"
+L_SHIFT="0x7000000E1"
+R_COMMAND="0x7000000E7"
+L_CONTROL="0x7000000E0"
+
+Map ${CAPS_LOCK} ${ESCAPE}
+#Map ${SECTION} ${ESCAPE}
+#Map ${R_COMMAND} ${SHIFT_LOCK}
+#Map ${BACKQUOTE} ${L_CONTROL}
+
+hidutil property --set "{\"UserKeyMapping\":[${CMD}]}"
+
+
 
 
 # These key repeat settings unfortunately don't take effect until logging out and logging back in.  Where are these settings persisted?
@@ -18,4 +42,4 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool FALSE # Disable Pre
 defaults write NSGlobalDomain InitialKeyRepeat -int 10 # Set key repeat triggering delay to even shorter.
 defaults write NSGlobalDomain KeyRepeat -int 3 # Set key repeat rate to even faster.
 
-hostname > ~/yep_it_worked
+hostname > ~/hmm_it_ran
